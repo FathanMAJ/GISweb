351fc427825094b7157453d6538ac2d74f5df899
fathanMAJ <fathanmusyaffa73@gmail.com>
2023-05-24 22:18:24 +0700

    edit header dark dan edit judul page

--- application/views/layout/viewnav.php
+++ application/views/layout/viewnav.php
@@ -1,73 +1,72 @@
-<div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="index3.html" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Contact</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Dropdown</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="#" class="dropdown-item">Some action </a></li>
-              <li><a href="#" class="dropdown-item">Some other action</a></li>
+<html lang="en">
+<head>
+<meta charset="utf-8">
 
-              <li class="dropdown-divider"></li>
+<meta name="viewport" content="width=device-width, initial-
+scale=1">
 
-              <!-- Level two dropdown-->
-              <li class="dropdown-submenu dropdown-hover">
-                <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
-                <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item">level 2</a>
-                  </li>
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>AdminLTE 3 | Top Navigation</title>
+<!-- Font Awesome Icons -->
+<link rel="stylesheet"
 
-                  <!-- Level three dropdown-->
-                  <li class="dropdown-submenu">
-                    <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
-                    <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                    </ul>
-                  </li>
-                  <!-- End Level three -->
+href="<?=base_url()?>assets/template/plugins/fontawesome-
+free/css/all.min.css">
 
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                </ul>
-              </li>
-              <!-- End Level two -->
-            </ul>
-          </li>
-        </ul>
+<!-- Theme style -->
+<link rel="stylesheet"
+href="<?=base_url()?>assets/template/dist/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+<link
+href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300
+,400,400i,700"
+rel="stylesheet">
+</head>
+<div class="collapse navbar-collapse order-3"
+id="navbarCollapse">
+<!-- Left navbar links -->
+<ul class="navbar-nav">
+<li class="nav-item">
+<a href="#" style= "color:white" class="nav-link">Home</a>
+</li>
+<li class="nav-item">
+<a href="#" style= "color:white" class="nav-link">Map</a>
+</li>
+<li class="nav-item dropdown">
 
-        <!-- SEARCH FORM -->
-      </div>
-    </nav>
-  <!-- /.navbar -->
+<a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-
+haspopup="true" aria expanded="false" style= "color:white"
 
-  <!-- Content Wrapper. Contains page content -->
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item"><a href="#">Layout</a></li>
-              <li class="breadcrumb-item active">Top Navigation</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+class="nav-link dropdown-toggle">Download</a>
+<ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu
+border-0 shadow">
+
+<li><a href="<?=base_url()?>assets/images.zip" class="dropdown-
+item">images.zip </a></li>
+
+<li><a href="<?=base_url()?>assets/images2.zip"class="dropdown-
+item">images2.zip </a></li>
+
+<li><a href="<?=base_url()?>assets/images3.zip"class="dropdown-
+item">images3.zip </a></li>
+
+<li class="dropdown-divider"></li>
+<!-- End Level two -->
+</ul>
+</li>
+</ul>
+<!-- SEARCH FORM -->
+</div>
+</nav>
+<!-- /.navbar -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+<!-- Control sidebar content goes here -->
+<div class="p-3">
+<h5>Title</h5>
+<p>Sidebar content</p>
+</div>
+</aside>
