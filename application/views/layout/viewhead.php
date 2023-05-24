8133427deeb328de2d6d26cf415498a056340051
fathanMAJ <fathanmusyaffa73@gmail.com>
2023-05-24 21:02:35 +0700

    nambahin peta inset

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -34,4 +34,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <link rel="stylesheet"
 href="<?=base_url()?>assets/leaflet.groupedlayercontrol.css" />
 <script src="<?=base_url()?>assets/leaflet.groupedlayercontrol.js"></script>
+
+
+<link rel="stylesheet" href="<?=base_url()?>assets/Control.MiniMap.css" />
+<script src="<?=base_url()?>assets/Control.MiniMap.js"></script>
+
 </head>
